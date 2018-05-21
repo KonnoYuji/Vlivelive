58637d310d9def2c6ae99425f55b0d04e9e35ecf
YujiKonno <b3lypscfcon@gmail.com>
2018-05-21 19:20:05 +0900

    初期位置を調整するUIを作成、OCulus GoのUIを調整

---
+++ Assets/TrackCameraOnce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackCameraOnce : MonoBehaviour {
+
+    [SerializeField]
+    private Transform Controller;
+
+    private Vector3 diffVector;
+
+    private void Awake()
+    {
+        diffVector = transform.position - Controller.position;
+    }
+
+    private void Update()
+    {
+        //OVRInput.Update();
+
+        //if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+        //{
+        //    transform.position = Camera.main.transform.forward;
+        //}
+
+        transform.position = Controller.position + diffVector;
+    }
+
+}
---
+++ Assets/offSetInput.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class offSetInput : MonoBehaviour {
+
+    public Transform CameraRig;
+
+    public TrackCamera trackCamera;
+
+    private bool settedTrackCamera = false;
+
+    public void UpCameraRig()
+    {
+        if(trackCamera == null)
+        {
+            return;
+        }
+
+        CameraRig.position += new Vector3(0, 0.1f, 0);
+    }
+
+    public void DownCameraRig()
+    {
+        if (trackCamera == null)
+        {
+            return;
+        }
+
+        CameraRig.position += new Vector3(0, -0.1f, 0);
+    }
+
+    public void LeftCameraRig()
+    {
+        if (trackCamera == null)
+        {
+            return;
+        }
+        CameraRig.position += new Vector3(-0.1f, 0, 0);
+    }
+
+    public void RightCameraRig()
+    {
+        if (trackCamera == null)
+        {
+            return;
+        }
+        CameraRig.position += new Vector3(0.1f, 0, 0);
+    }
+
+    public void ForwardCameraRig()
+    {
+        if (trackCamera == null)
+        {
+            return;
+        }
+        CameraRig.position += new Vector3(0, 0, 0.1f);
+    }
+
+    public void BackCameraRig()
+    {
+        if (trackCamera == null)
+        {
+            return;
+        }
+        CameraRig.position += new Vector3(0, 0, -0.1f);
+    }
+
+    public void FinishOffset()
+    {
+        if (trackCamera == null)
+        {
+            return;
+        }
+
+        trackCamera.settedOffset = true;
+        this.gameObject.SetActive(false);
+    }
+}
