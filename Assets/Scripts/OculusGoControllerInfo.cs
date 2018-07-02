1b43caef7ddf0ad2903348690e8f6158ae395943
YujiKonno <b3lypscfcon@gmail.com>
2018-07-02 17:43:22 +0900

    Quickまわりのデータ取得、認識方法を確立

--- Assets/Scripts/OculusGoControllerInfo.cs
+++ Assets/Scripts/OculusGoControllerInfo.cs
@@ -8,8 +8,13 @@ public class OculusGoControllerInfo : MonoBehaviour {
 
 	private StringBuilder data = new StringBuilder();
 
+	private string rotVel;
+
 	[SerializeField]
 	private Text uiText;
+
+	[SerializeField]
+	private Text rotVelocityText;
 
 	private bool isRunning = false;
 
@@ -23,6 +28,13 @@ public class OculusGoControllerInfo : MonoBehaviour {
 		}
 	}
 
+	private bool isHighAcceraration = false;
+
+	private bool quickReel = false;
+	
+	private bool quickStretch = false;
+
+	private float passedTime = 0;
 	void Update () {
 
 		primaryTouchpad = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
@@ -44,11 +56,34 @@ public class OculusGoControllerInfo : MonoBehaviour {
 		// Quaternion rot = OVRInput.GetLocalControllerRotation(activeController);
 		// data.AppendFormat("Orientation: ({0:F2}, {1:F2}, {2:F2}, {3:F2})\n", rot.x, rot.y, rot.z, rot.w);
 
-		// Vector3 angVel = OVRInput.GetLocalControllerAngularVelocity(activeController);
-		// data.AppendFormat("AngVel: ({0:F2}, {1:F2}, {2:F2})\n", angVel.x, angVel.y, angVel.z);
+		Vector3 angVel = OVRInput.GetLocalControllerAngularVelocity(activeController);
+		data.AppendFormat("AngVel: ({0:F2}, {1:F2}, {2:F2})\n", angVel.x, angVel.y, angVel.z);
+		
+		Vector3 angAcc = OVRInput.GetLocalControllerAngularAcceleration(activeController);
+		data.AppendFormat("AngAcc: ({0:F2}, {1:F2}, {2:F2})\n", angAcc.x, angAcc.y, angAcc.z);
+		
+		if(angAcc.x > 100 && !isHighAcceraration)
+		{
+			rotVel += "AngAcc over 100 : " + angAcc.x + '\n';		
+			isHighAcceraration = true;
+		}
 
-		// Vector3 angAcc = OVRInput.GetLocalControllerAngularAcceleration(activeController);
-		// data.AppendFormat("AngAcc: ({0:F2}, {1:F2}, {2:F2})\n", angAcc.x, angAcc.y, angAcc.z);
+		if(isHighAcceraration)
+		{
+			StartCoroutine(CheckQuickReelMotion());
+			StartCoroutine(CheckQuickStretchMotion());
+		}
+		
+		// if(angAcc.x > 150 && ( Mathf.Abs(rot.x) <= 0.05f && Mathf.Abs(rot.x) >= 0))
+		// {
+		// 	rotVel += "AngAcc over 100 : " + angAcc.x + '\n';
+		// 	rotVel += "Horizon\n";			
+		// }
+		// else if(angAcc.x > 150 && ( Mathf.Abs(rot.x) <= 0.55f && Mathf.Abs(rot.x) >= 0.45f))
+		// {
+		// 	rotVel += "AngAcc over 100 : " + angAcc.x + '\n';
+		// 	rotVel += "Vertical\n";			
+		// }
 
 		// Vector3 pos = OVRInput.GetLocalControllerPosition(activeController);
 		// data.AppendFormat("Position: ({0:F2}, {1:F2}, {2:F2})\n", pos.x, pos.y, pos.z);
@@ -73,7 +108,50 @@ public class OculusGoControllerInfo : MonoBehaviour {
 			uiText.text = data.ToString();
 		}
 
-		yield return new WaitForSeconds(0.25f);
+		if(rotVelocityText)
+		{
+			rotVelocityText.text = rotVel;
+		}
+		//yield return new WaitForSeconds(0.25f);
+		yield return null;
 		isRunning = false;		
+	}
+
+	private IEnumerator CheckQuickReelMotion()
+	{
+		yield return new WaitForSeconds(0.25f);
+
+		var ctrForward = this.transform.forward.normalized;
+		data.AppendFormat("ctrForward: ({0:F2}, {1:F2}, {2:F2})\n", ctrForward.x, ctrForward.y, ctrForward.z);
+		var dot = Vector3.Dot(ctrForward, Vector3.up);
+		var sita = Mathf.Acos(dot) * (180 / 3.14f);
+
+		if((sita <= 20 && sita >= 0))
+		{
+			quickStretch = false;
+			quickReel  = true;
+			Debug.Log("QuickReel is true");
+		}
+
+		isHighAcceraration = false;		
+	}
+
+	private IEnumerator CheckQuickStretchMotion()
+	{
+		yield return new WaitForSeconds(0.25f);
+
+		var ctrForward = this.transform.forward.normalized;
+		data.AppendFormat("ctrForward: ({0:F2}, {1:F2}, {2:F2})\n", ctrForward.x, ctrForward.y, ctrForward.z);
+		var dot = Vector3.Dot(ctrForward, Vector3.up);
+		var sita = Mathf.Acos(dot) * (180 / 3.14f);
+
+		if(sita >= 70 && 110 >= sita)
+		{			
+			quickReel  = false;
+			quickStretch = true;
+			Debug.Log("QuickStretch is true");
+		}
+
+		isHighAcceraration = false;
 	}
 }
